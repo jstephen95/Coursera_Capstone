3ffa6f6a1fa43ba944d0b06354a17ad22ef423e3
unknown <stephenjoshua64@mail.com>
2020-03-26 13:50:58 +0100

    adds a new notebook to Github

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone Project
+# ### This Notebook will be used mainly for Capstone Projects
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
